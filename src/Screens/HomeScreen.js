9cbb6b689f0fe3fd6cba5cc82ab04a4b99b945ab
SofiaMD <61211590+SofiaMD@users.noreply.github.com>
2020-11-01 17:03:13 -0600

    Pull de la barra de busqueda

--- src/Screens/HomeScreen.js
+++ src/Screens/HomeScreen.js
@@ -1,19 +1,33 @@
 //Importar librerias y modulos necesarios.
 import React, { Component } from "react";
-import {StyleSheet, Text} from "react-native";
-//import {Container} from "native-base";
+import {requireNativeComponent, StyleSheet, Text, Image, Dimensions} from "react-native";
+import {Input,Container, Item} from "native-base";
 
+const { width, height} = Dimensions.get("window");
 
 //Variable que contiene la pantalla
 const HomeScreen = () => {
     return (
-        <Text>Pantalla Principal</Text>
-);
+        <Container>
+            <Image source={require("../../assets/Letras.png")} styles={styles.gifterImage}/>
+            <Text>Pantalla Principal</Text>
+        </Container>
+        
+    );
 };
 
 //Variable para la hoja de estilos
 const styles = StyleSheet.create({
-   
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    gifterImage: {
+    width: width,
+    height: height * 0.30,
+    resizeMode: "contain",
+    }  
 
 });
 
