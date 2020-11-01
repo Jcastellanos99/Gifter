acda06e33e9407b8fd5cb862666ef285056e1288
Jcastellanos99 <karolcastellanos99@gmail.com>
2020-11-01 14:28:06 -0600

    Inicio proyecto, aplicacion Gifter

---
+++ src/Screens/HomeScreen.js
@@ -0,0 +1,20 @@
+//Importar librerias y modulos necesarios.
+import React, { Component } from "react";
+import {StyleSheet, Text} from "react-native";
+//import {Container} from "native-base";
+
+
+//Variable que contiene la pantalla
+const HomeScreen = () => {
+    return (
+        <Text>Pantalla Principal</Text>
+);
+};
+
+//Variable para la hoja de estilos
+const styles = StyleSheet.create({
+   
+
+});
+
+export default HomeScreen;
