1ba3d698aa27db232b1ad6751ec2aa5854e36629
Jcastellanos99 <karolcastellanos99@gmail.com>
2020-11-01 16:56:33 -0600

    Creación del diseño de la barra y boton de busqueda

--- src/Screens/HomeScreen.js
+++ src/Screens/HomeScreen.js
@@ -1,19 +1,43 @@
 //Importar librerias y modulos necesarios.
-import React, { Component } from "react";
-import {StyleSheet, Text} from "react-native";
-//import {Container} from "native-base";
+import React from "react";
+import {StyleSheet, Text, View} from "react-native";
+import {Container,
+        Item,
+        Input,
+        Header,
+        Icon,
+        Right,
+        Button
+        } from "native-base";
 
 
 //Variable que contiene la pantalla
 const HomeScreen = () => {
     return (
-        <Text>Pantalla Principal</Text>
-);
+        <Container>
+            <Header searchBar style={styles.search}>
+                <Item>
+                    <Input placeholder="Buscar"/>
+                    <Right>
+                        <Button transparent><Icon name="search"/></Button>
+                    </Right>
+                </Item>
+            </Header>                   
+        </Container>
+    );
 };
 
 //Variable para la hoja de estilos
 const styles = StyleSheet.create({
-   
+    search:
+    {
+        backgroundColor:"white",
+        borderRadius: 160/2,
+        marginLeft:15,
+        marginRight:15,
+        marginTop:10,
+        height: 40,
+    },
 
 });
 
