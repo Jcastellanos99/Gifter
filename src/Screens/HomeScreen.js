a0017aaea3b044453b41bda810584b485a440b9c
Jcastellanos99 <karolcastellanos99@gmail.com>
2020-11-01 20:55:10 -0600

    Creación de los botones que daran las opciones de busqueda y modificación de la barra de busqueda

--- src/Screens/HomeScreen.js
+++ src/Screens/HomeScreen.js
@@ -1,5 +1,5 @@
 //Importar librerias y modulos necesarios.
-import React from "react";
+import React, { useState } from "react";
 import {StyleSheet, Text, View} from "react-native";
 import {Container,
         Item,
@@ -7,22 +7,54 @@ import {Container,
         Header,
         Icon,
         Right,
-        Button
+        Button,
+        Segment
         } from "native-base";
+import backend from "../api/backend";
+import getEnvVars from "../../enviroment";
+
+
+const {apiKey} = getEnvVars();
+
 
 
 //Variable que contiene la pantalla
 const HomeScreen = () => {
+    //Maneja el estado de los stickers.
+    const [stickers, setStickers] = useState(null);
+
+    const getStickers = async () => {
+        //Se consulta la api
+        const response = await backend.get(`stickers/trending?api_key=${apiKey}&limit=25&rating=g`);
+
+        console.log(response.data);
+    }
+
+    getStickers();
+
+
     return (
         <Container>
-            <Header searchBar style={styles.search}>
-                <Item>
+            <Header searchBar noShadow style={styles.search}>
+                <Item rounded>
                     <Input placeholder="Buscar"/>
                     <Right>
-                        <Button transparent><Icon name="search"/></Button>
+                        <Button transparent>
+                            <Icon name="search"/>
+                        </Button>
                     </Right>
                 </Item>
-            </Header>                   
+            </Header>  
+            <Segment style={styles.segment}>
+                <Button rounded style={styles.button}>
+                    <Text>Stickers</Text>
+                </Button>
+                <Button rounded style={styles.button}>
+                    <Text>
+                        Gifs
+                    </Text>
+                </Button>
+            </Segment>          
         </Container>
     );
 };
@@ -32,11 +64,18 @@ const styles = StyleSheet.create({
     search:
     {
         backgroundColor:"white",
-        borderRadius: 160/2,
-        marginLeft:15,
-        marginRight:15,
-        marginTop:10,
-        height: 40,
+        marginTop:5,
+        
+    },
+    segment:
+    {
+        backgroundColor: "white",
+    },
+    button:
+    {
+        borderBottomColor: "gray",
+        marginLeft: 55,
+        marginRight: 55,
     },
 
 });
