626521d59761d19a015e414c8a80f6ed2fd9e8fb
Jcastellanos99 <karolcastellanos99@gmail.com>
2020-11-01 20:52:00 -0600

    configuración para la conectividad con la API

---
+++ src/api/backend.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+import getEnvVars from "../../enviroment";
+
+const { apiUrl } = getEnvVars();
+
+//Instacia de conexión
+const instance = axios.create({
+    baseURL: apiUrl
+});
+
+export default instance;
