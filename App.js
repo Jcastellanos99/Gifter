d7b2d222dfb1e1360009417f60b842f9205a31f8
Jcastellanos99 <karolcastellanos99@gmail.com>
2020-11-09 23:24:29 -0600

    Llamado de las 3 pantallas actuales mediante react navigation

--- App.js
+++ App.js
@@ -1,8 +1,39 @@
 import React from 'react';
+import {Image, Dimensions} from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 import HomeScreen from "./src/Screens/HomeScreen";
+import SearchResults from "./src/Screens/SearchResults";
+import ShowImageScreen from "./src/Screens/ShowImageScreen";
+import { Form } from 'native-base';
+
+const { width, height} = Dimensions.get("window");
+
+//Crear nuestra navegacion.
+const Stack = createStackNavigator();
+
+function LogoTitle() {
+  return (
+    <Image
+      style={{ width: 50, height: 50 }}
+      source={require("./assets/Logo.png")}
+    />
+  );
+}
+
 
 export default function App() {
-  return <HomeScreen></HomeScreen>;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="homeScreen">
+        <Stack.Screen name="homeScreen" component={HomeScreen} options={{headerStyle: {backgroundColor: "#EDE9E6"}, 
+      headerTitle: props => <LogoTitle {...props} />}} />
+        <Stack.Screen name="searchResults" component={SearchResults} options={{headerStyle: {backgroundColor: "#EDE9E6"}}}/>
+        <Stack.Screen name="Image" component={ShowImageScreen} options={{headerStyle: {backgroundColor: "#EDE9E6"}}}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
+
 
 
