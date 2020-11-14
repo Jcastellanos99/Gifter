bd7288d62f6ab7afebb1c6fcacc3ceec537f47b9
Jcastellanos99 <karolcastellanos99@gmail.com>
2020-11-14 00:42:50 -0600

    Llamamiento de la nueva pantalla al react navigation

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import {Image, Dimensions} from "react-native";
+import {Image, Dimensions, StyleSheet} from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import HomeScreen from "./src/Screens/HomeScreen";
 import SearchResults from "./src/Screens/SearchResults";
 import ShowImageScreen from "./src/Screens/ShowImageScreen";
+import HomeScreenGif from "./src/Screens/HomeScreenGif"
 import { Form } from 'native-base';
 
 const { width, height} = Dimensions.get("window");
@@ -15,8 +16,27 @@ const Stack = createStackNavigator();
 function LogoTitle() {
   return (
     <Image
-      style={{ width: 50, height: 50 }}
-      source={require("./assets/Logo.png")}
+      source={require("./assets/Logo1.png")}
+      style={styles.LogoImage}  
+    />
+  );
+}
+
+
+function LogoTitleGif() {
+  return (
+    <Image
+      source={require("./assets/Logo1.png")}
+      style={styles.LogoImageGif}  
+    />
+  );
+}
+
+function ImageTitle() {
+  return (
+    <Image
+      source={require("./assets/Logo1.png")}
+      style={styles.gifterImage}  
     />
   );
 }
@@ -26,14 +46,49 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="homeScreen">
-        <Stack.Screen name="homeScreen" component={HomeScreen} options={{headerStyle: {backgroundColor: "#EDE9E6"}, 
-      headerTitle: props => <LogoTitle {...props} />}} />
-        <Stack.Screen name="searchResults" component={SearchResults} options={{headerStyle: {backgroundColor: "#EDE9E6"}}}/>
-        <Stack.Screen name="Image" component={ShowImageScreen} options={{headerStyle: {backgroundColor: "#EDE9E6"}}}/>
+        <Stack.Screen name="homeScreen" component={HomeScreen} 
+        options={{headerTitle: props=> <LogoTitle {...props}/>, headerStyle: {backgroundColor: "#251C69",}}} />
+         <Stack.Screen name="homeScreenGif" component={HomeScreenGif} 
+        options={{headerTitle: props=> <LogoTitleGif {...props}/>, headerStyle: {backgroundColor: "#251C69",}}} />
+        <Stack.Screen name="searchResults" component={SearchResults} 
+        options={{headerTitle: props=> <ImageTitle {...props}/>, headerStyle: {backgroundColor: "#251C69"}, headerTintColor: "#fff"}}/>
+        <Stack.Screen name="Image" component={ShowImageScreen} 
+        options={{headerTitle: props=> <ImageTitle {...props}/>, headerStyle: {backgroundColor: "#251C69"}, headerTintColor: "#fff"}}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
 
+
+const styles = StyleSheet.create({
+  
+  LogoImage: 
+    {
+        width: width,
+        height: height * 0.05,
+        marginLeft: width*-0.045,
+        resizeMode: "contain",
+        flex : 1,
+        backgroundColor: "#251C69",
+    },
+    LogoImageGif: 
+    {
+        width: width,
+        height: height * 0.05,
+        marginLeft: width*-0.18,
+        resizeMode: "contain",
+        flex : 1,
+        backgroundColor: "#251C69",
+    },
+    gifterImage: 
+    {
+        width: width*0.68,
+        height: height * 0.05,
+        
+        resizeMode: "contain",
+        flex : 1,
+        backgroundColor: "#251C69",
+    },
+})
 
 
